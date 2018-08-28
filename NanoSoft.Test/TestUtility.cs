24cae4394088d1bf20817e05bbb0596f9e299a55
Emadem <Emad@EMAD-PC>
2018-08-28 22:48:54 +0200

    TestUtility can now handle SqlServer (v1.0.5)

--- NanoSoft.Test/TestUtility.cs
+++ NanoSoft.Test/TestUtility.cs
@@ -8,11 +8,11 @@ namespace NanoSoft.Test
     public abstract class TestUtility<TApplication, TUserInfo, TUnitOfWork, TDbContext> : IDisposable
         where TDbContext : DbContext
     {
-        public static bool InMemoryDb = true;
+        public bool InMemoryDb { get; protected set; } = true;
+        public string ConnectionString { get; protected set; }
         private readonly string _dbName;
 
         protected abstract void Migrate(DbContext context);
-        protected abstract DbContextOptionsBuilder RegisterProvider(DbContextOptionsBuilder builder);
         protected abstract TDbContext Initialize(DbContextOptions options);
         protected abstract TUnitOfWork Initialize(TDbContext context);
 
@@ -42,7 +42,7 @@ namespace NanoSoft.Test
 
             builder = InMemoryDb
                 ? builder.UseInMemoryDatabase($"NanoSoft_{dbName}")
-                : RegisterProvider(builder);
+                : builder.UseSqlServer(ConnectionString);
 
             return Initialize(builder.Options);
         }

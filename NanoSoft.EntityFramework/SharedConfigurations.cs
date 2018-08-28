925ad5162409cd752f434b61bd45e6a1cc7cce01
Emadem <Emad@EMAD-PC>
2018-08-28 02:00:03 +0200

    Extend SharedConfigurations (v1.0.1)

--- NanoSoft.EntityFramework/SharedConfigurations.cs
+++ NanoSoft.EntityFramework/SharedConfigurations.cs
@@ -1,5 +1,74 @@
-﻿namespace NanoSoft.EntityFramework
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using NanoSoft.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NanoSoft.EntityFramework
 {
+    public class SharedConfigurations<TEntity, TDbContext> : SharedConfigurations
+        where TEntity : class
+    {
+        [NotNull]
+        public virtual EntityValidationState IsValid(IEnumerable<EntityEntry<TEntity>> entityEntries, TDbContext context)
+        {
+            EntityValidationState validationState = null;
+
+            foreach (var entityEntry in entityEntries.ToList())
+            {
+                switch (entityEntry.State)
+                {
+                    case EntityState.Detached:
+                        break;
+
+                    case EntityState.Unchanged:
+                        CheckUnchanged(entityEntry, context);
+                        break;
+
+                    case EntityState.Added:
+                        validationState = ValidToAdd(entityEntry, context);
+                        break;
+
+                    case EntityState.Modified:
+                        validationState = ValidToModify(entityEntry, context);
+                        break;
+
+                    case EntityState.Deleted:
+                        validationState = ValidToDelete(entityEntry, context);
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(entityEntry.State));
+                }
+
+                if (validationState == null)
+                    continue;
+
+                if (!validationState.IsValid)
+                    return validationState;
+            }
+
+            return EntityValidationState.Valid;
+        }
+
+        protected virtual void CheckUnchanged(EntityEntry<TEntity> entityEntry, TDbContext context)
+        {
+
+        }
+
+
+        protected virtual EntityValidationState ValidToAdd(EntityEntry<TEntity> entityEntry, TDbContext context)
+            => EntityValidationState.Valid;
+
+        protected virtual EntityValidationState ValidToModify(EntityEntry<TEntity> entityEntry, TDbContext context)
+            => EntityValidationState.Valid;
+
+        protected virtual EntityValidationState ValidToDelete(EntityEntry<TEntity> entityEntry, TDbContext context)
+            => EntityValidationState.Valid;
+    }
+
     public class SharedConfigurations
     {
         public const string DecimalField = "decimal(18, 3)";

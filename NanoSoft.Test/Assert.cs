6e1cf41c596e37518ca315814791bf3ed0ccf480
Emadem <Emad@EMAD-PC>
2018-08-30 21:03:04 +0200

    Improve Test Assertion (v1.1.1)

--- NanoSoft.Test/Assert.cs
+++ NanoSoft.Test/Assert.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 namespace NanoSoft.Test
 {
@@ -7,8 +6,23 @@ namespace NanoSoft.Test
     {
         public static void Equal<T>(T expected, T actual1, T actual2)
         {
-            if (!actual1.Equals(expected) || !actual2.Equals(expected))
-                throw new Exception("failed to assert that both actual values equals expected");
+            try
+            {
+                Xunit.Assert.Equal(expected, actual1);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("expected != actual1, " + e.Message);
+            }
+
+            try
+            {
+                Xunit.Assert.Equal(expected, actual2);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("expected != actual2, " + e.Message);
+            }
         }
     }
 }

ddd302f6300e57148c5fb2573803ba1d4a74c3cf
Emadem <Emad@EMAD-PC>
2018-09-04 20:58:49 +0200

    RelayCommand support async (v1.0.5)

--- NanoSoft.Wpf/Mvvm/RelayCommand.cs
+++ NanoSoft.Wpf/Mvvm/RelayCommand.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace NanoSoft.Wpf.Mvvm
 {
     public class RelayCommand : ICommand
     {
+        private readonly Func<Task> _executeAsync;
         private readonly Action _execute;
         private readonly Func<bool> _canExecute;
 
@@ -20,9 +22,20 @@ namespace NanoSoft.Wpf.Mvvm
             _canExecute = canExecute;
         }
 
+        public RelayCommand(Func<Task> executeAsync, Func<bool> canExecute)
+        {
+            _executeAsync = executeAsync;
+            _canExecute = canExecute;
+        }
+
         public RelayCommand(Action execute)
         {
             _execute = execute;
+        }
+
+        public RelayCommand(Func<Task> executeAsync)
+        {
+            _executeAsync = executeAsync;
         }
 
 
@@ -31,15 +44,25 @@ namespace NanoSoft.Wpf.Mvvm
             return _canExecute == null || _canExecute();
         }
 
-        public void Execute(object parameter)
+        public async void Execute(object parameter)
         {
+            if (_executeAsync != null)
+            {
+                await _executeAsync();
+                return;
+            }
+
             _execute();
         }
+
+        public Task ExecuteAsync() => _executeAsync();
     }
+
     public class RelayCommand<T> : ICommand
     {
         #region Fields
 
+        private readonly Func<T, Task> _executeAsync;
         private readonly Action<T> _execute;
         private readonly Predicate<T> _canExecute;
 
@@ -55,6 +78,12 @@ namespace NanoSoft.Wpf.Mvvm
         public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Func<T, Task> executeAsync, Predicate<T> canExecute = null)
+        {
+            _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
             _canExecute = canExecute;
         }
 
@@ -87,11 +116,18 @@ namespace NanoSoft.Wpf.Mvvm
         ///Defines the method to be called when the command is invoked.
         ///</summary>
         ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
-        public void Execute(object parameter)
+        public async void Execute(object parameter)
         {
+            if (_executeAsync != null)
+            {
+                await _executeAsync((T)parameter);
+                return;
+            }
+
             _execute((T)parameter);
         }
 
+        public Task ExecuteAsync(object parameter) => _executeAsync((T)parameter);
         #endregion
     }
 

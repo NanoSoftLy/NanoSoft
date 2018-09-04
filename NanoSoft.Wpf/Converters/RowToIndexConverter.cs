74f9ef79ca01b500b2e7e97fde50c5534a39a0c6
Emadem <Emad@EMAD-PC>
2018-09-04 06:03:02 +0200

    Add RowToIndex Converter (v1.0.1)

---
+++ NanoSoft.Wpf/Converters/RowToIndexConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace NanoSoft.Wpf.Converters
+{
+    public class RowToIndexConverter : MarkupExtension, IValueConverter
+    {
+        private static RowToIndexConverter _converter;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => value is DataGridRow row ? row.GetIndex() + 1 : -1;
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+            => 0;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+            => _converter ?? (_converter = new RowToIndexConverter());
+    }
+}

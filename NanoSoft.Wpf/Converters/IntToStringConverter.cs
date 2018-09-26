409ef9d966e836f68c6636725f106ac0546364a2
Admin <admin@nanosoft.ly>
2018-09-26 14:25:56 +0200

    Implement int converter and shared wpf extensions (v1.1.1)

---
+++ NanoSoft.Wpf/Converters/IntToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace NanoSoft.Wpf.Converters
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public IntToStringReturnValue ReturnValue { get; set; } = IntToStringReturnValue.Null;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) => value?.ToString();
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var parsed = int.TryParse(value?.ToString(), out var intVal);
+
+            if (parsed)
+                return intVal;
+
+            Console.WriteLine($"ConvertBack to int failed in {nameof(IntToStringConverter)}, value = {value}");
+            return ReturnValue == IntToStringReturnValue.Zero ? 0 : (int?)null;
+        }
+    }
+
+
+    public enum IntToStringReturnValue
+    {
+        Null,
+        Zero,
+    }
+}
---
+++ NanoSoft.Wpf/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows.Media.Imaging;
+
+namespace NanoSoft.Wpf
+{
+    public static class Extensions
+    {
+        public static BitmapImage ToBitmapImage(this byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+
+        public static string ToUnsecureString(this SecureString securePassword)
+        {
+            if (securePassword == null)
+            {
+                return string.Empty;
+            }
+
+            var unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+    }
+}

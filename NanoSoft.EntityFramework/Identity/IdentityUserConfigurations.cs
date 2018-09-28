7db7814129735fa68d92e52b79414186a1ddb411
Admin <admin@nanosoft.ly>
2018-09-29 01:18:58 +0200

    Configure Identity Table (v1.1.1)

--- NanoSoft.EntityFramework/Identity/IdentityUserConfigurations.cs
+++ NanoSoft.EntityFramework/Identity/IdentityUserConfigurations.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NanoSoft.Identity;
+using NanoSoft.Repository;
+using NanoSoft.Resources;
+using System.Linq;
 
 namespace NanoSoft.EntityFramework.Identity
 {
@@ -10,6 +14,27 @@ namespace NanoSoft.EntityFramework.Identity
         {
             builder.Property(e => e.Name).IsRequired().HasMaxLength(SmallField);
             builder.Property(e => e.Password).IsRequired().HasMaxLength(SmallField);
+        }
+
+
+        protected override EntityValidationState ValidToAdd(EntityEntry<TIdentityUser> entityEntry, NanoSoftIdentityDbContext<TIdentityUser> context)
+        {
+            var entity = entityEntry.Entity;
+
+            if (context.Identities.Any(i => i.Name == entity.Name))
+                return EntityValidationState.Error(e => entity.Name, SharedMessages.NameExisted);
+
+            return base.ValidToAdd(entityEntry, context);
+        }
+
+        protected override EntityValidationState ValidToModify(EntityEntry<TIdentityUser> entityEntry, NanoSoftIdentityDbContext<TIdentityUser> context)
+        {
+            var entity = entityEntry.Entity;
+
+            if (context.Identities.Any(i => i.Name == entity.Name && i.Id != entity.Id))
+                return EntityValidationState.Error(e => entity.Name, SharedMessages.NameExisted);
+
+            return base.ValidToModify(entityEntry, context);
         }
     }
 }

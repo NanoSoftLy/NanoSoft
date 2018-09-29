b1470c601bc15e39f1177dedeeb50f63dd03d120
Admin <admin@nanosoft.ly>
2018-09-29 23:30:49 +0200

    Implement IIdentityRepository (v1.2.0)

---
+++ NanoSoft/Identity/IIdentityRepository.cs
@@ -0,0 +1,11 @@
+﻿using NanoSoft.Repository;
+using System.Threading.Tasks;
+
+namespace NanoSoft.Identity
+{
+    public interface IIdentityRepository<TIdentityUser> : IRepository<TIdentityUser>
+        where TIdentityUser : class
+    {
+        Task<TIdentityUser> GetByNameAndPasswordAsync(string name, string password);
+    }
+}
--- NanoSoft/Identity/IIdentityUnitOfWork.cs
+++ NanoSoft/Identity/IIdentityUnitOfWork.cs
@@ -2,7 +2,9 @@
 
 namespace NanoSoft.Identity
 {
-    public interface IIdentityUnitOfWork<out TIdentityRepository> : IDefaultUnitOfWork
+    public interface IIdentityUnitOfWork<out TIdentityRepository, TIdentityUser> : IDefaultUnitOfWork
+        where TIdentityRepository : IIdentityRepository<TIdentityUser>
+        where TIdentityUser : class
     {
         TIdentityRepository IdentityUsers { get; }
     }

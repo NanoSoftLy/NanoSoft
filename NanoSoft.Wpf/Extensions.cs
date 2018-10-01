b99ae18c02058170caa2f6f8e3917f98219dcbc8
Admin <admin@nanosoft.ly>
2018-10-01 08:59:41 +0200

    Implement Language Extensions (v1.2.5)

--- NanoSoft.Wpf/Extensions.cs
+++ NanoSoft.Wpf/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NanoSoft.Wpf.Components;
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -8,6 +9,50 @@ namespace NanoSoft.Wpf
 {
     public static class Extensions
     {
+        public static string GetCulture(this Language language)
+        {
+            switch (language)
+            {
+                case Language.Arabic:
+                    return "ar-LY";
+                case Language.English:
+                    return "en-US";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(language));
+            }
+        }
+
+
+        public static Language GetLanguage(this string culture)
+        {
+            if (string.IsNullOrWhiteSpace(culture))
+                return Language.Arabic;
+
+            switch (culture)
+            {
+                case "en-US":
+                    return Language.English;
+
+                case "ar-LY":
+                    return Language.Arabic;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(culture));
+            }
+        }
+
+        public static DateTime? ArrangeDate(this DateTime? from, DateTime? to)
+        {
+            if (from == null)
+                return to;
+
+            if (to != null && to >= from)
+                return to;
+
+            return from;
+        }
+
+
         public static BitmapImage ToBitmapImage(this byte[] imageData)
         {
             if (imageData == null || imageData.Length == 0) return null;
